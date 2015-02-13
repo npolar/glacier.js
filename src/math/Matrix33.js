5e613dc9180026f75ef379897afc20982f178d79
Remi Aleksander Solås <remi@npolar.no>
2015-02-13 17:05:21 +0100

    Added Matrix33 and Matrix44 classes

---
+++ src/math/Matrix33.js
@@ -0,0 +1,123 @@
+glacier.Matrix33 = function(value) {
+	this.array = new Float32Array([
+		1.0, 0.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 0.0, 1.0
+	]);
+	
+	if(value) {
+		this.assign(value);
+	}
+};
+
+glacier.Matrix33.prototype = {
+	assign: function(value) {
+		var row, col, e;
+		
+		if(value instanceof glacier.Matrix33) {
+			for(e in value.array) {
+				this.array[e] = value.array[e];
+			}
+		} else if(value instanceof glacier.Matrix44) {
+			for(col = 0; col < 3; ++col) {
+				for(row = 0; row < 3; ++row) {
+					this.array[(col * 3) + row] = value.array[(col * 4) + row];
+				}
+			}
+		} else if(value instanceof Array || value instanceof Float32Array) {
+			for(e in value) {
+				this.array[e] = value[e];
+			}
+		} else if(typeof value == 'number') {
+			for(e in this.array) {
+				this.array[e] = value;
+			}
+		} else {
+			console.warn('Invalid parameter type for glacier.Matrix33.assign: ' + typeof(value) + ' (expected Matrix33, Matrix44, array or number)');
+		}
+	},
+	
+	determinant: function() {
+		return (this.array[0] * (this.array[4] * this.array[8] - this.array[5] * this.array[7]) -
+				this.array[1] * (this.array[3] * this.array[8] - this.array[5] * this.array[6]) +
+				this.array[2] * (this.array[3] * this.array[7] - this.array[4] * this.array[6]));
+	},
+	
+	element: function(col, row) {
+		return this.array[(col * 3) + row];
+	},
+	
+	multiply: function(value) {
+		var col, row, e, temp;
+		
+		if(value instanceof glacier.Matrix33) {
+			temp = new Float32Array(9);
+			
+			for(col = 0; col < 3; ++col) {
+				for(row = 0; row < 3; ++row) {
+					temp[(col * 3) + row] = ((this.array[(col * 3) + 0] * value.array[(0 * 3) + row]) +
+											 (this.array[(col * 3) + 1] * value.array[(1 * 3) + row]) +
+											 (this.array[(col * 3) + 2] * value.array[(2 * 3) + row]));
+				}
+			}
+		} else if(value instanceof glacier.Matrix44) {
+			temp = new glacier.Matrix33(value);
+			this.multiply(temp);
+		} else if(typeof value == 'number') {
+			for(e in this.array) {
+				this.array[e] *= value;
+			}
+		} else {
+			console.warn('Invalid parameter type for glacier.Matrix33.multiply: ' + typeof(value) + ' (expected Matrix33, Matrix44 or number)');
+		}
+	},
+	
+	transpose: function() {
+		var temp;
+		
+		temp = this.array[1];
+		this.array[1] = this.array[3];
+		this.array[3] = temp;
+		
+		temp = this.array[2];
+		this.array[2] = this.array[6];
+		this.array[6] = temp;
+		
+		temp = this.array[5];
+		this.array[5] = this.array[7];
+		this.array[7] = temp;
+	},
+	
+	invert: function() {
+		var temp = new Float32Array([
+			this.array[4] * this.array[8] - this.array[5] * this.array[7],
+			this.array[2] * this.array[7] - this.array[1] * this.array[8],
+			this.array[1] * this.array[5] - this.array[2] * this.array[4],
+			this.array[5] * this.array[6] - this.array[3] * this.array[8],
+			this.array[0] * this.array[8] - this.array[2] * this.array[6],
+			this.array[2] * this.array[3] - this.array[0] * this.array[5],
+			this.array[3] * this.array[7] - this.array[4] * this.array[6],
+			this.array[1] * this.array[6] - this.array[0] * this.array[7],
+			this.array[0] * this.array[4] - this.array[1] * this.array[3]
+		]);
+		
+		var det = (this.array[0] * temp[0] - this.array[1] * temp[3] + this.array[2] * temp[6]);
+		
+		if(Math.abs(det) < Number.EPSILON)
+			return false;
+		
+		det = 1.0 / det;
+		
+		for(var e in temp) {
+			this.array[e] = (temp[e] * det);
+		}
+		
+		return true;
+	},
+	
+	toString: function() {
+		return ('[[' + this.array[0] + ', ' + this.array[1] + ', ' + this.array[2] + '], ' +
+				 '[' + this.array[3] + ', ' + this.array[4] + ', ' + this.array[5] + '], ' +
+				 '[' + this.array[6] + ', ' + this.array[7] + ', ' + this.array[8] + ']]');
+	}
+};
---
+++ src/math/Matrix44.js
@@ -0,0 +1,167 @@
+glacier.Matrix44 = function(value) {
+	this.array = new Float32Array([
+		1.0, 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, 0.0, 1.0
+	]);
+	
+	if(value) {
+		this.assign(value);
+	}
+};
+
+glacier.Matrix44.prototype = {
+	assign: function(value) {
+		var row, col, e;
+		
+		if(value instanceof glacier.Matrix33) {
+			for(col = 0; col < 3; ++col) {
+				for(row = 0; row < 3; ++row) {
+					this.array[(col * 4) + row] = value.array[(col * 3) + row];
+				}
+			}
+			
+			this.array[ 3] = this.array[ 7] = this.array[11] = 0.0;
+			this.array[12] = this.array[13] = this.array[14] = 0.0;
+			this.array[16] = 1.0;
+		} else if(value instanceof glacier.Matrix44) {
+			for(e in value.array) {
+				this.array[e] = value.array[e];
+			}
+		} else if(value instanceof Array || value instanceof Float32Array) {
+			for(e in value) {
+				this.array[e] = value[e];
+			}
+		} else if(typeof value == 'number') {
+			for(e in this.array) {
+				this.array[e] = value;
+			}
+		} else {
+			console.warn('Invalid parameter type for glacier.Matrix44.assign: ' + typeof(value) + ' (expected Matrix33, Matrix44, array or number)');
+		}
+	},
+	
+	determinant: function() {
+		var a0 = this.array[ 0] * this.array[ 5] - this.array[ 1] * this.array[ 4];
+		var a1 = this.array[ 0] * this.array[ 6] - this.array[ 2] * this.array[ 4];
+		var a2 = this.array[ 0] * this.array[ 7] - this.array[ 3] * this.array[ 4];
+		var a3 = this.array[ 1] * this.array[ 6] - this.array[ 2] * this.array[ 5];
+		var a4 = this.array[ 1] * this.array[ 7] - this.array[ 3] * this.array[ 5];
+		var a5 = this.array[ 2] * this.array[ 7] - this.array[ 3] * this.array[ 6];
+		var b0 = this.array[ 8] * this.array[13] - this.array[ 9] * this.array[12];
+		var b1 = this.array[ 8] * this.array[14] - this.array[10] * this.array[12];
+		var b2 = this.array[ 8] * this.array[15] - this.array[11] * this.array[12];
+		var b3 = this.array[ 9] * this.array[14] - this.array[11] * this.array[13];
+		var b4 = this.array[ 9] * this.array[15] - this.array[10] * this.array[13];
+		var b5 = this.array[10] * this.array[15] - this.array[11] * this.array[14];
+		
+		return (a0 * b5 - a1 * b4 + a2 * b3 + a3 * b2 - a4 * b1 + a5 * b0); 
+	},
+
+	element: function(col, row) {
+		return this.array[(col * 4) + row];
+	},
+	
+	multiply: function() {
+		var col, row, e, temp;
+		
+		if(value instanceof glacier.Matrix44) {
+			temp = new Float32Array(16);
+			
+			for(col = 0; col < 4; ++col) {
+				for(row = 0; row < 4; ++row) {
+					temp[(col * 4) + row] = ((this.array[(col * 4) + 0] * value.array[(0 * 4) + row]) +
+											 (this.array[(col * 4) + 1] * value.array[(1 * 4) + row]) +
+											 (this.array[(col * 4) + 2] * value.array[(2 * 4) + row]) +
+											 (this.array[(col * 4) + 3] * value.array[(3 * 4) + row]));
+				}
+			}
+		} else if(value instanceof glacier.Matrix33) {
+			temp = new glacier.Matrix44(value);
+			this.multiply(temp);
+		} else if(typeof value == 'number') {
+			for(e in this.array) {
+				this.array[e] *= value;
+			}
+		} else {
+			console.warn('Invalid parameter type for glacier.Matrix4.multiply: ' + typeof(value) + ' (expected Matrix44, Matrix33 or number)');
+		}
+	},
+	
+	transpose: function() {
+		var temp;
+		
+		temp = this.array[1];
+		this.array[1] = this.array[4];
+		this.array[4] = temp;
+		
+		temp = this.array[2];
+		this.array[2] = this.array[8];
+		this.array[8] = temp;
+		
+		temp = this.array[3];
+		this.array[3] = this.array[12];
+		this.array[12] = temp;
+		
+		temp = this.array[6];
+		this.array[6] = this.array[9];
+		this.array[9] = temp;
+		
+		temp = this.array[7];
+		this.array[7] = this.array[13];
+		this.array[13] = temp;
+		
+		temp = this.array[11];
+		this.array[11] = this.array[14];
+		this.array[14] = temp;
+	},
+	
+	invert: function() {
+		var a0 = this.array[ 0] * this.array[ 5] - this.array[ 1] * this.array[ 4];
+		var a1 = this.array[ 0] * this.array[ 6] - this.array[ 2] * this.array[ 4];
+		var a2 = this.array[ 0] * this.array[ 7] - this.array[ 3] * this.array[ 4];
+		var a3 = this.array[ 1] * this.array[ 6] - this.array[ 2] * this.array[ 5];
+		var a4 = this.array[ 1] * this.array[ 7] - this.array[ 3] * this.array[ 5];
+		var a5 = this.array[ 2] * this.array[ 7] - this.array[ 3] * this.array[ 6];
+		var b0 = this.array[ 8] * this.array[13] - this.array[ 9] * this.array[12];
+		var b1 = this.array[ 8] * this.array[14] - this.array[10] * this.array[12];
+		var b2 = this.array[ 8] * this.array[15] - this.array[11] * this.array[12];
+		var b3 = this.array[ 9] * this.array[14] - this.array[11] * this.array[13];
+		var b4 = this.array[ 9] * this.array[15] - this.array[10] * this.array[13];
+		var b5 = this.array[10] * this.array[15] - this.array[11] * this.array[14];
+		
+		var det = (a0 * b5 - a1 * b4 + a2 * b3 + a3 * b2 - a4 * b1 + a5 * b0); 
+		
+		if(Math.abs(det) < Number.EPSILON)
+			return false;
+		
+		this.array = new Float32Array([
+			 this.array[ 5] * b5 - this.array[ 6] * b4 + this.array[ 7] * b3,
+			-this.array[ 1] * b5 + this.array[ 2] * b4 - this.array[ 3] * b3,
+			 this.array[13] * a5 - this.array[14] * a4 + this.array[15] * a3,
+			-this.array[ 9] * a5 + this.array[10] * a4 - this.array[11] * a3,
+			-this.array[ 4] * b5 + this.array[ 6] * b2 - this.array[ 7] * b1,
+			 this.array[ 0] * b5 - this.array[ 2] * b2 + this.array[ 3] * b1,
+			-this.array[12] * a5 + this.array[14] * a2 - this.array[15] * a1,
+			 this.array[ 8] * a5 - this.array[10] * a2 + this.array[11] * a1,
+			 this.array[ 4] * b4 - this.array[ 5] * b2 + this.array[ 7] * b0,
+			-this.array[ 0] * b4 + this.array[ 1] * b2 - this.array[ 3] * b0,
+			 this.array[12] * a4 - this.array[13] * a2 + this.array[15] * a0,
+			-this.array[ 8] * a4 + this.array[ 9] * a2 - this.array[11] * a0,
+			-this.array[ 4] * b3 + this.array[ 5] * b1 - this.array[ 6] * b0,
+			 this.array[ 0] * b3 - this.array[ 1] * b1 + this.array[ 2] * b0,
+			-this.array[12] * a3 + this.array[13] * a1 - this.array[14] * a0,
+			 this.array[ 8] * a3 - this.array[ 9] * a1 + this.array[10] * a0 
+		]);
+		
+		return true;
+	},
+	
+	toString: function() {
+		return ('[[' + this.array[ 0] + ', ' + this.array[ 1] + ', ' + this.array[ 2] + ', ' + this.array[ 3] + '], ' +
+				 '[' + this.array[ 4] + ', ' + this.array[ 5] + ', ' + this.array[ 6] + ', ' + this.array[ 7] + '], ' +
+				 '[' + this.array[ 8] + ', ' + this.array[ 9] + ', ' + this.array[10] + ', ' + this.array[11] + '], ' +
+				 '[' + this.array[12] + ', ' + this.array[13] + ', ' + this.array[14] + ', ' + this.array[15] + ']]');
+	}
+};
