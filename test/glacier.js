b6d2c66e8cc167241cd45841206e07b97d441eec
Remi Aleksander Solås <remi@npolar.no>
2015-02-16 17:38:01 +0100

    Added test for glacier.compare function

--- test/glacier.js
+++ test/glacier.js
@@ -1,2 +1,12 @@
 var assert = require('assert');
 var glacier = require('../dist/glacier.min.js');
+
+describe('glacier', function() {
+	it('compare', function() {
+		var n1 = 123.45678, n2 = 123.45678, n3 = 123.456, n4 = 123;
+		
+		assert.equal(true, glacier.compare(n1, n2));
+		assert.equal(false, glacier.compare(n2, n3));
+		assert.equal(false, glacier.compare(n3, n4));
+	});
+});
--- test/test.js
+++ test/test.js
@@ -1,6 +1,16 @@
 var assert = require('assert');
 var glacier = require('../dist/glacier.min.js');
 
+describe('glacier', function() {
+	it('compare', function() {
+		var n1 = 123.45678, n2 = 123.45678, n3 = 123.456, n4 = 123;
+		
+		assert.equal(true, glacier.compare(n1, n2));
+		assert.equal(false, glacier.compare(n2, n3));
+		assert.equal(false, glacier.compare(n3, n4));
+	});
+});
+
 describe('Vector2', function() {
 	it('class in glacier', function() {
 		assert.equal('object', typeof glacier);
