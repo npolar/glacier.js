b71f75fdd9bb337fcc393deb684ce151801e257e
Remi Aleksander Solås <remi@npolar.no>
2015-04-08 16:37:12 +0200

    Added a few more debug messages

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,4 +1,5 @@
 glacier.i18n.en = {
+	CONTEXT_ERROR:		'{context} context error: {error}',
 	INDEX_OUT_OF_RANGE:	'Index out of range: {index} (expected range {range}) in {method}',
 	INVALID_ASSIGNMENT:	'Invalid assigment of {variable}: {value} (expected {expected})',
 	INVALID_PARAMETER:	'Invalid parameter {parameter}: {value} (expected {expected}) in {method}',
@@ -7,5 +8,6 @@ glacier.i18n.en = {
 	UNDEFINED_ELEMENT:	'Undefined element ID: {element} in {method}',
 	UNDEFINED_ERROR:	'Undefined error: {error}',
 	UNDEFINED_WARNING:	'Undefined warning: {warning}',
-	UNDEFINED_LANGUAGE:	'Undefined language: {language} (using {fallback} as fallback)'
+	UNDEFINED_LANGUAGE:	'Undefined language: {language} (using {fallback} as fallback)',
+	UNKNOWN_PROPERTY:	'Unrecognized property: {property} in {object}'
 };
--- src/i18n/nb.js
+++ src/i18n/nb.js
@@ -1,4 +1,5 @@
 glacier.i18n.nb = {
+	CONTEXT_ERROR:		'{context} kontekstfeil: {error}',
 	INDEX_OUT_OF_RANGE:	'Index out of range: {index} (expected range {range}) in {method}',
 	INVALID_ASSIGNMENT:	'Ugyldig tildeing av {variable}: {value} (forventet {expected})',
 	INVALID_PARAMETER:	'Ugyldig parameter {parameter}: {value} (forventet {expected}) i {method}',
@@ -7,5 +8,6 @@ glacier.i18n.nb = {
 	UNDEFINED_ELEMENT:	'Udefinert element ID: {element} i {method}',
 	UNDEFINED_ERROR:	'Udefinert feilmelding: {error}',
 	UNDEFINED_WARNING:	'Udefinert advarsel: {warning}',
-	UNDEFINED_LANGUAGE:	'Udefinert språkkode: {language} (bruker {fallback})'
+	UNDEFINED_LANGUAGE:	'Udefinert språkkode: {language} (bruker {fallback})',
+	UNKNOWN_PROPERTY:	'Ukjent egenskap: {property} i {object}'
 };
