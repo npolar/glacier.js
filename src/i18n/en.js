d024909ab8491b8179953deefccc3b5351bd153c
Remi Aleksander Solås <remi@npolar.no>
2015-04-14 18:33:34 +0200

    Added new debug logs

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,13 +1,14 @@
 glacier.i18n.en = {
-	CONTEXT_ERROR:		'{context} context error: {error}',
-	INDEX_OUT_OF_RANGE:	'Index out of range: {index} (expected range {range}) in {method}',
-	INVALID_ASSIGNMENT:	'Invalid assigment of {variable}: {value} (expected {expected})',
-	INVALID_PARAMETER:	'Invalid parameter {parameter}: {value} (expected {expected}) in {method}',
-	MATRIX_NO_INVERSE:	'Inverse matrix does not exist: {matrix} in {method}',
-	MISSING_PARAMETER:	'Missing mandatory parameter: {parameter} in {method}',
-	UNDEFINED_ELEMENT:	'Undefined element ID: {element} in {method}',
-	UNDEFINED_ERROR:	'Undefined error: {error}',
-	UNDEFINED_WARNING:	'Undefined warning: {warning}',
-	UNDEFINED_LANGUAGE:	'Undefined language: {language} (using {fallback} as fallback)',
-	UNKNOWN_PROPERTY:	'Unrecognized property: {property} in {object}'
+	CONTEXT_ERROR:			'{context} context error: {error}',
+	INDEX_OUT_OF_RANGE:		'Index out of range: {index} (expected range {range}) in {method}',
+	INVALID_ASSIGNMENT:		'Invalid assigment of {variable}: {value} (expected {expected})',
+	INVALID_PARAMETER:		'Invalid parameter {parameter}: {value} (expected {expected}) in {method}',
+	MATRIX_NO_INVERSE:		'Inverse matrix does not exist: {matrix} in {method}',
+	MISSING_IMPLEMENTATION:	'Missing implementation: {implementation} in {child} extension of {parent}',
+	MISSING_PARAMETER:		'Missing mandatory parameter: {parameter} in {method}',
+	UNDEFINED_ELEMENT:		'Undefined element ID: {element} in {method}',
+	UNDEFINED_ERROR:		'Undefined error: {error}',
+	UNDEFINED_WARNING:		'Undefined warning: {warning}',
+	UNDEFINED_LANGUAGE:		'Undefined language: {language} (using {fallback} as fallback)',
+	UNKNOWN_PROPERTY:		'Unrecognized property: {property} in {object}'
 };
