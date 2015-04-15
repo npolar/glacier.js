aac52ac8b2979d53e68a858911a6229fd40c5f6a
Remi Aleksander Solås <remi@npolar.no>
2015-04-15 17:56:05 +0200

    Added context-relationship and the use of TypedArray to Mesh class

--- src/Mesh.js
+++ src/Mesh.js
@@ -1,37 +1,46 @@
 glacier.Mesh = function(context) {
+	if(context && !(context instanceof glacier.Context)) {
+		glacier.error('INVALID_PARAMETER', { parameter: 'context', value: typeof context, expected: 'Context', method: 'Mesh constructor' });
+	} else {
+		context = (context instanceof glacier.Context ? context : null);
+	}
+	
+	Object.defineProperties(this, {
+		context: {
+			get: function() {
+				return context;
+			},
+			set: function(value) {
+				if(value instanceof glacier.Context) {
+					context = value;
+				} else if(context) {
+					glacier.error('INVALID_ASSIGNMENT', { variable: 'Mesh.context', value: typeof value, expected: 'Context' });
+				} else {
+					context = null;
+				}
+			}
+		},
+		
+		colors: 	{ value: new glacier.TypedArray('Color', glacier.Color) },
+		indices:	{ value: new glacier.TypedArray('number') },
+		normals:	{ value: new glacier.TypedArray('Vector3', glacier.Vector3) },
+		texCoords:	{ value: new glacier.TypedArray('Vector2', glacier.Vector2) },
+		vertices:	{ value: new glacier.TypedArray('Vector3', glacier.Vector3) }
+	});
 };
 
 glacier.Mesh.prototype = {
-	generateSphere: function(latitudes, longitudes, radius) {
-		var sphere = new glacier.Sphere(latitudes, longitudes, radius);
-		
-		if(sphere.radius) {
-			this.indices = sphere.indices;
-			this.normals = sphere.normals;
-			this.uvCoords = sphere.uvCoords;
-			this.vertices = sphere.vertices;
-			return true;
+	destroy: function() {
+		this.colors.length		= 0;
+		this.indices.length	 	= 0;
+		this.normals.length		= 0;
+		this.texCoords.length	= 0;
+		this.vertices.length	= 0;
+	},
+	
+	draw: function() {
+		if(context) {
+			context.draw(this);
 		}
-		
-		return false;
 	}
 };
-
-'indices,normals,uvCoords,vertices'.split(',').forEach(function(property, index) {
-	var a, array = [];
-	
-	Object.defineProperty(glacier.Mesh.prototype, property, {
-		get: function() {
-			return array;
-		},
-		set: function(value) {
-			if(glacier.isArray(value)) {
-				for(a = 0; a < value.length; ++a) {
-					array.push(value[a]);
-				}
-			} else {
-				glacier.error('INVALID_ASSIGNMENT', { variable: 'Mesh.' + property, value: typeof value, expected: 'array' });
-			}
-		}
-	});
-});
