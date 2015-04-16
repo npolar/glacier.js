4bf588253f96984f11af4289888de46e274c3606
Remi Aleksander Solås <remi@npolar.no>
2015-04-16 19:08:12 +0200

    Updated Mesh class to inherit Drawable

--- src/Mesh.js
+++ src/Mesh.js
@@ -1,26 +1,9 @@
-glacier.Mesh = function(context) {
-	if(context && !(context instanceof glacier.Context)) {
-		glacier.error('INVALID_PARAMETER', { parameter: 'context', value: typeof context, expected: 'Context', method: 'Mesh constructor' });
-	} else {
-		context = (context instanceof glacier.Context ? context : null);
-	}
-	
+glacier.Mesh = function Mesh() {
+	// Call super constructor
+	glacier.Drawable.call(this);
+
+	// Define buffer arrays
 	Object.defineProperties(this, {
-		context: {
-			get: function() {
-				return context;
-			},
-			set: function(value) {
-				if(value instanceof glacier.Context) {
-					context = value;
-				} else if(context) {
-					glacier.error('INVALID_ASSIGNMENT', { variable: 'Mesh.context', value: typeof value, expected: 'Context' });
-				} else {
-					context = null;
-				}
-			}
-		},
-		
 		colors: 	{ value: new glacier.TypedArray('Color', glacier.Color) },
 		indices:	{ value: new glacier.TypedArray('number') },
 		normals:	{ value: new glacier.TypedArray('Vector3', glacier.Vector3) },
@@ -29,17 +12,15 @@ glacier.Mesh = function(context) {
 	});
 };
 
-glacier.Mesh.prototype = {
-	destroy: function() {
+// glacier.Mesh extends glacier.Drawable
+glacier.extend(glacier.Mesh, glacier.Drawable, {
+	free: function() {
+		glacier.Drawable.prototype.free.call(this);
+		
 		this.colors.length		= 0;
 		this.indices.length	 	= 0;
 		this.normals.length		= 0;
 		this.texCoords.length	= 0;
 		this.vertices.length	= 0;
-	},
-	draw: function() {
-		if(context) {
-			context.draw(this);
-		}
 	}
-};
+});
