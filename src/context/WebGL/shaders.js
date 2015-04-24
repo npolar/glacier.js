b2cf2223c7a10ea8f916a89c48f35cbd8965f247
Remi Aleksander Solås <remi@npolar.no>
2015-04-24 17:04:10 +0200

    Updated GLSL source for new functionality

--- src/context/WebGL/shaders.js
+++ src/context/WebGL/shaders.js
@@ -1,6 +1,6 @@
 glacier.context.WebGL.shaders = {
 	vertex: {
-		general: [
+		generic: [
 			'attribute highp vec3 vertex_xyz;',
 			'attribute highp vec3 normal_xyz;',
 			'attribute highp vec2 texture_uv;',
@@ -8,42 +8,74 @@ glacier.context.WebGL.shaders = {
 			'uniform highp mat4 matrix_mvp;',
 			'varying mediump vec2 tex_coords;',
 			'varying mediump vec4 frag_color;',
+			'varying mediump vec3 mvp_normal;',
 			'void main()',
 			'{',
+				'gl_PointSize = 2.0;',
 				'gl_Position = matrix_mvp * vec4(vertex_xyz, 1.0);',
 				'tex_coords = texture_uv; frag_color = color_rgba;',
+				'mvp_normal = normalize(matrix_mvp * vec4(normal_xyz, 1.0)).xyz;',
 			'}'
 		]
 	},
 	fragment: {
-		textured: [
-			'precision mediump float;',
-			'uniform sampler2D sampler_texture;',
-			'varying mediump vec2 tex_coords;',
+		generic: [
 			'varying mediump vec4 frag_color;',
 			'void main()',
 			'{',
-				'gl_FragColor = texture2D(sampler_texture, tex_coords);',
+				'gl_FragColor = frag_color;',
+			'}'
+		],
+		globe: [
+			'precision mediump float;',
+			'uniform sampler2D tex_samp_0;',
+			'uniform sampler2D tex_samp_1;',
+			'uniform sampler2D tex_samp_2;',
+			'varying mediump vec2 tex_coords;',
+			'varying mediump vec4 frag_color;',
+			'varying mediump vec3 mvp_normal;',
+			'void main()',
+			'{',
+				'vec3 lightPos = normalize(vec3(-28.0, 2.0, 12.0));',
+				'vec3 normal = normalize(texture2D(tex_samp_2, tex_coords).rgb * 2.0 - 1.0);',
+				'float diffuse = max(dot(mvp_normal, lightPos), 0.0);',
+				'vec3 dayColor = texture2D(tex_samp_0, tex_coords).rgb * diffuse;',
+				'vec3 nightColor = texture2D(tex_samp_1, tex_coords).rgb * (1.0 - diffuse);',
+				//'gl_FragColor = vec4(dayColor + nightColor, 1.0);',
+				
+				'gl_FragColor = vec4(nightColor + dayColor * max(dot(normal, lightPos), 0.3), 1.0);',
 			'}'
 		],
 		normalMapped: [
 			'precision mediump float;',
-			'uniform sampler2D sampler_texture;',
-			'uniform sampler2D sampler_normal_map;',
+			'uniform sampler2D tex_samp_0;',
+			'uniform sampler2D tex_samp_1;',
 			'varying mediump vec2 tex_coords;',
 			'varying mediump vec4 frag_color;',
 			'void main()',
 			'{',
 				'vec3 lightPos = normalize(vec3(1.0, 1.0, 1.0));',
 				'vec4 fragColor = vec4(1.0, 1.0, 1.0, 1.0);',
-				'vec3 normal = normalize(texture2D(sampler_normal_map, tex_coords).rgb * 2.0 - 1.0);',
+				'vec3 normal = normalize(texture2D(tex_samp_1, tex_coords).rgb * 2.0 - 1.0);',
 				'float diffuse = max(dot(normal, lightPos), 0.0);',
-				'gl_FragColor = vec4(diffuse * texture2D(sampler_texture, tex_coords).rgb, 1.0);',
+				'gl_FragColor = vec4(diffuse * texture2D(tex_samp_0, tex_coords).rgb, 1.0);',
+			'}'
+		],
+		textured: [
+			'precision mediump float;',
+			'uniform sampler2D tex_samp_0;',
+			'varying mediump vec2 tex_coords;',
+			'varying mediump vec4 frag_color;',
+			'void main()',
+			'{',
+				'gl_FragColor = texture2D(tex_samp_0, tex_coords);',
 			'}'
 		]
 	},
 	programs: {
-		textured: { vertex: 'general', fragment: 'textured' },
-		normalMapped: { vertex: 'general', fragment: 'normalMapped' }
+		generic: { vertex: 'generic', fragment: 'generic' },
+		globe: { vertex: 'generic', fragment: 'globe' },
+		normalMapped: { vertex: 'generic', fragment: 'normalMapped' },
+		textured: { vertex: 'generic', fragment: 'textured' }
 	}
 };
