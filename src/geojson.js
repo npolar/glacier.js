70c5f93a859c1123e12630bf25d62a5230b7939f
Remi Aleksander Solås <remi@npolar.no>
2015-05-15 11:55:50 +0200

    Added FeatureCollection and GeometryCollection as Classes of geoJSON

--- src/geojson.js
+++ src/geojson.js
@@ -1,13 +1,17 @@
 (function() {
 	var geoJSON = {
-		Point: function(lat, lng, alt) {
-			this.lat = (typeof lat == 'number' ? lat : 0.0);
-			this.lng = (typeof lng == 'number' ? lng : 0.0);
-			this.alt = (typeof alt == 'number' ? alt : undefined);
+		Feature: function(geometry, properties, id) {
+			this.id = (id !== undefined ? id : null);
+			this.geometry = (geometry || null);
+			this.properties = (typeof properties == 'object' ? properties : {});
 		},
 		
-		MultiPoint: function(points) {
-			this.points = (points instanceof Array ? points : []);
+		FeatureCollection: function(features) {
+			this.features = (features instanceof Array ? features : []);
+		},
+		
+		GeometryCollection: function(geometries) {
+			this.geometries = (geometries instanceof Array ? geometries : []);
 		},
 		
 		LineString: function(points) {
@@ -18,52 +22,43 @@
 			this.lineStrings = (lineStrings instanceof Array ? lineStrings : []);
 		},
 		
-		Polygon: function(rings) {
-			this.rings = (rings instanceof Array ? rings : []);
+		MultiPoint: function(points) {
+			this.points = (points instanceof Array ? points : []);
 		},
 		
 		MultiPolygon: function(polygons) {
 			this.polygons = (polygons instanceof Array ? polygons : []);
 		},
 		
-		Feature: function(geometry, properties, id) {
-			this.id = (id !== undefined ? id : null);
-			this.geometry = (geometry || null);
-			this.properties = (typeof properties == 'object' ? properties : {});
+		Point: function(lat, lng, alt) {
+			this.lat = (typeof lat == 'number' ? lat : 0.0);
+			this.lng = (typeof lng == 'number' ? lng : 0.0);
+			this.alt = (typeof alt == 'number' ? alt : undefined);
 		},
 		
-		parsePoint: function(coords) {
-			if(coords instanceof Array) {
-				var point = {
-					lng: (typeof coords[0] == 'number' ? coords[0] : undefined),
-					lat: (typeof coords[1] == 'number' ? coords[1] : undefined),
-					alt: (typeof coords[2] == 'number' ? coords[2] : undefined)
-				};
+		Polygon: function(rings) {
+			this.rings = (rings instanceof Array ? rings : []);
+		},
+		
+		parse: function(string) {
+			var geojson, data;
+			
+			try { geojson = JSON.parse(string); }
+			catch(e) { return null; }
+			
+			return geoJSON.parseObject(geojson);
+		},
+		
+		parseGeometry: function(geometryObject) {
+			if(typeof geometryObject == 'object') {
+				var object, geometries = 'Point,MultiPoint,LineString,MultiLineString,Polygon,MultiPolygon,GeometryCollection'.split(',');
 				
-				if(point.lat !== undefined && point.lng !== undefined) {
-					return new geoJSON.Point(point.lat, point.lng, point.alt);
+				if(geometries.indexOf(geometryObject.type) != -1) {
+					return geoJSON.parseObject(geometryObject);
 				}
 			}
 			
-			return null; // Invalid Point
-		},
-		
-		parseMultiPoint: function(points) {
-			if(points instanceof Array) {
-				var multiPoint = [], p;
-				
-				for(p in points) {
-					if((p = geoJSON.parsePoint(points[p]))) {
-						multiPoint.push(p);
-					}
-				}
-				
-				if(multiPoint.length == points.length) {
-					return new geoJSON.MultiPoint(multiPoint);
-				}
-			}
-			
-			return null; // Invalid MultiPoint
+			return null; // Invalid Geometry
 		},
 		
 		parseLineString: function(points) {
@@ -104,24 +99,22 @@
 			return null; // Invalid MultiLineString
 		},
 		
-		parsePolygon: function(rings) {
-			if(rings instanceof Array) {
-				var polygon = [], r, points;
+		parseMultiPoint: function(points) {
+			if(points instanceof Array) {
+				var multiPoint = [], p;
 				
-				for(r in rings) {
-					if((r = geoJSON.parseLineString(rings[r]))) {
-						if((points = r.length) >= 4 && r[0].compare(r[points - 1])) {
-							polygon.push(r);
-						}
+				for(p in points) {
+					if((p = geoJSON.parsePoint(points[p]))) {
+						multiPoint.push(p);
 					}
 				}
 				
-				if(polygon.length == rings.length) {
-					return new geoJSON.Polygon(polygon);
+				if(multiPoint.length == points.length) {
+					return new geoJSON.MultiPoint(multiPoint);
 				}
 			}
 			
-			return null; // Invalid Polygon
+			return null; // Invalid MultiPoint
 		},
 		
 		parseMultiPolygon: function(polygons) {
@@ -140,72 +133,6 @@
 			}
 			
 			return null; // Invalid MultiPolygon
-		},
-		
-		parseGeometry: function(geometryObject) {
-			if(typeof geometryObject == 'object') {
-				var object, geometries = 'Point,MultiPoint,LineString,MultiLineString,Polygon,MultiPolygon,GeometryCollection'.split(',');
-				
-				if(geometries.indexOf(geometryObject.type) != -1) {
-					return geoJSON.parseObject(geometryObject);
-				}
-			}
-			
-			return null; // Invalid Geometry
-		},
-		
-		parseGeometryCollection: function(geometries) {
-			if(geometries instanceof Array) {
-				var geometryCollection = [], g;
-				
-				for(g in geometries) {
-					if((g = geoJSON.parseGeometry(geometries[g]))) {
-						geometryCollection.push(g);
-					}
-				}
-				
-				if(geometryCollection.length == geometries.length) {
-					return geometryCollection;
-				}
-			}
-			
-			return null; // Invalid GeometryCollection
-		},
-		
-		parseFeature: function(featureObject) {
-			if(typeof featureObject == 'object' && featureObject.type == 'Feature') {
-				var geometry, properties, id = featureObject.id;
-				
-				if(featureObject.geometry !== null && !(geometry = geoJSON.parseGeometry(featureObject.geometry))) {
-					return null; // Invalid geometry member
-				}
-				
-				if((properties = featureObject.properties) !== null && typeof properties != 'object') {
-					return null; // Invalid properties member
-				}
-				
-				return new geoJSON.Feature(geometry, properties, id);
-			}
-			
-			return null; // Invalid Feature
-		},
-		
-		parseFeatureCollection: function(features) {
-			if(features instanceof Array) {
-				var featureCollection = [], f;
-				
-				for(f in features) {
-					if((f = geoJSON.parseFeature(features[f]))) {
-						featureCollection.push(f);
-					}
-				}
-				
-				if(featureCollection.length == features.length) {
-					return featureCollection;
-				}
-			}
-			
-			return null; // Invalid FeatureCollection
 		},
 		
 		parseObject: function(object) {
@@ -263,13 +190,94 @@
 			return null; // Invalid GeoJSON object
 		},
 		
-		parse: function(string) {
-			var geojson, data;
+		parsePoint: function(coords) {
+			if(coords instanceof Array) {
+				var point = {
+					lng: (typeof coords[0] == 'number' ? coords[0] : undefined),
+					lat: (typeof coords[1] == 'number' ? coords[1] : undefined),
+					alt: (typeof coords[2] == 'number' ? coords[2] : undefined)
+				};
+				
+				if(point.lat !== undefined && point.lng !== undefined) {
+					return new geoJSON.Point(point.lat, point.lng, point.alt);
+				}
+			}
 			
-			try { geojson = JSON.parse(string); }
-			catch(e) { return null; }
+			return null; // Invalid Point
+		},
+		
+		parsePolygon: function(rings) {
+			if(rings instanceof Array) {
+				var polygon = [], r, points;
+				
+				for(r in rings) {
+					if((r = geoJSON.parseLineString(rings[r]))) {
+						if((points = r.length) >= 4 && r[0].compare(r[points - 1])) {
+							polygon.push(r);
+						}
+					}
+				}
+				
+				if(polygon.length == rings.length) {
+					return new geoJSON.Polygon(polygon);
+				}
+			}
 			
-			return geoJSON.parseObject(geojson);
+			return null; // Invalid Polygon
+		},
+		
+		parseGeometryCollection: function(geometries) {
+			if(geometries instanceof Array) {
+				var geometryCollection = [], g;
+				
+				for(g in geometries) {
+					if((g = geoJSON.parseGeometry(geometries[g]))) {
+						geometryCollection.push(g);
+					}
+				}
+				
+				if(geometryCollection.length == geometries.length) {
+					return new geoJSON.GeometryCollection(geometryCollection);
+				}
+			}
+			
+			return null; // Invalid GeometryCollection
+		},
+		
+		parseFeature: function(featureObject) {
+			if(typeof featureObject == 'object' && featureObject.type == 'Feature') {
+				var geometry, properties, id = featureObject.id;
+				
+				if(featureObject.geometry !== null && !(geometry = geoJSON.parseGeometry(featureObject.geometry))) {
+					return null; // Invalid geometry member
+				}
+				
+				if((properties = featureObject.properties) !== null && typeof properties != 'object') {
+					return null; // Invalid properties member
+				}
+				
+				return new geoJSON.Feature(geometry, properties, id);
+			}
+			
+			return null; // Invalid Feature
+		},
+		
+		parseFeatureCollection: function(features) {
+			if(features instanceof Array) {
+				var featureCollection = [], f;
+				
+				for(f in features) {
+					if((f = geoJSON.parseFeature(features[f]))) {
+						featureCollection.push(f);
+					}
+				}
+				
+				if(featureCollection.length == features.length) {
+					return new geoJSON.FeatureCollection(featureCollection);
+				}
+			}
+			
+			return null; // Invalid FeatureCollection
 		}
 	};
 	
